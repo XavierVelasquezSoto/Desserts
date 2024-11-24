a0f6ef5c71ce92b5e92e91cbdca1e7eb3767b462
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-24 19:57:18 +0100

    effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-const optionsFilter = document.getElementById("options-filter");
+const optionsFilterElement = document.getElementById("options-filter");
+const containerObjetcsElement = document.getElementById("container-objetcs");
 
 const filterSelection = (event) => {
   event.target.dataset.filters;
@@ -7,10 +8,31 @@ const filterSelection = (event) => {
     return;
   }
 
-  for (const filters of optionsFilter.children) {
+  for (const filters of optionsFilterElement.children) {
     filters.classList.remove("filter-desserts-active");
   }
   event.target.classList.add("filter-desserts-active");
 };
 
-optionsFilter.addEventListener("click", filterSelection);
+const containerObjetcs = (event) => {
+  const button = event.target.dataset.button;
+  //console.log(button);
+  if (!button) return;
+
+  if (button === "add") {
+    showEventsButton(event.target);
+    //console.log(event.target.dataset);
+  }
+};
+
+const showEventsButton = (effectVisual) => {
+  const buttonHidden = effectVisual; //hidden button
+  buttonHidden.classList.add("cart-container-hidden");
+  const buttonShow = effectVisual.nextElementSibling; //show button
+  buttonShow.classList.remove("cart-flex-hidden");
+  const imgBorder = effectVisual.previousElementSibling; //dessert selector border
+  imgBorder.classList.add("border-img-select");
+};
+
+optionsFilterElement.addEventListener("click", filterSelection);
+containerObjetcsElement.addEventListener("click", containerObjetcs);
