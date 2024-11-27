8284c656acbe1fae9181f4b26b9aa8698762cddb
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-27 16:58:05 +0100

    yourcart changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-const optionsFilterElement = document.getElementById("options-filter");
-const containerObjetcsElement = document.getElementById("container-objetcs");
-const cartProductsElement = document.getElementById("cart-products");
-const cartImgElement = document.getElementById("cart-img");
+const optionsFilterElement = document.getElementById('options-filter');
+const containerObjetcsElement = document.getElementById('container-objetcs');
+const cartProductsElement = document.getElementById('cart-products');
+const cartImgElement = document.getElementById('cart-img');
+const cartOrderTotalPriceElement = document.getElementById('cart-order-total-price');
+/* alternar comentario de linea = añadir tecla ctrl + }  si no lo tiene*/
 
 let cartContainer = [];
 
-const filterSelection = (event) => {
+const filterSelection = event => {
   event.target.dataset.filters;
   //console.log(event.target.dataset.filters);
   if (!event.target.dataset.filters) {
@@ -13,17 +15,32 @@ const filterSelection = (event) => {
   }
 
   for (const filters of optionsFilterElement.children) {
-    filters.classList.remove("filter-desserts-active");
+    filters.classList.remove('filter-desserts-active');
   }
-  event.target.classList.add("filter-desserts-active");
+  event.target.classList.add('filter-desserts-active');
+};
+
+const totalValorProducts = () => {
+  const totalProductsPrice = cartContainer.reduce((acc, product) => {
+    return acc + product.quantity;
+  });
+  cartOrderTotalPriceElement.textContent = product.quantity;
+  cartOrderTotalPriceElement.classList.remove('hide');
+};
+
+const removeEffectCartAdd = () => {
+  cartImgElement.classList.add('hide');
+};
+
+const addEffectCart = () => {
+  cartImgElement.classList.remove('hide');
 };
 
 const cartAddProductsText = () => {
-  /* cartContainer.textContent = `Your Cart (${quantity})`; */
-  const addYourCart = cartContainer.reduce((acc, quantity) => {
-    return acc + quantity;
-  });
-  return (cartProductsElement.textContent = addYourCart);
+  const totalProducts = cartContainer.reduce((acc, product) => {
+    return acc + product.quantity;
+  }, 0);
+  return (cartProductsElement.textContent = `Your Cart (${totalProducts})`);
 };
 
 const cartTextButton = (element, quantity) => {
@@ -35,17 +52,20 @@ const cartTextButton = (element, quantity) => {
 const addToCart = (name, price) => {
   //console.log(cartContainer);
   cartContainer.push({ name: name, price: price, quantity: 1 });
+  cartAddProductsText();
+  removeEffectCartAdd();
 };
 
-const removeFromCart = (name) => {
-  cartContainer = cartContainer.filter((productCart) => {
+const removeFromCart = name => {
+  cartContainer = cartContainer.filter(productCart => {
     return productCart.name !== name;
   });
+  cartAddProductsText();
   //console.log(cartContainer);
 };
 
 const increaseQuantity = (element, name) => {
-  const cartProduct = cartContainer.find((product) => {
+  const cartProduct = cartContainer.find(product => {
     return product.name === name;
   });
   cartProduct.quantity++;
@@ -56,42 +76,42 @@ const increaseQuantity = (element, name) => {
 
 const decreaseQuantity = (element, name) => {
   //console.log(element, name);
-  const cartProduct = cartContainer.find((product) => {
+  const cartProduct = cartContainer.find(product => {
     return product.name === name;
   });
 
   if (cartProduct.quantity === 1) {
-    element.previousElementSibling.classList.remove("cart-container-hidden");
-    element.previousElementSibling.previousElementSibling.classList.remove(
-      "border-img-select"
-    );
-    element.classList.add("hide");
+    element.previousElementSibling.classList.remove('cart-container-hidden');
+    element.previousElementSibling.previousElementSibling.classList.remove('border-img-select');
+    element.classList.add('hide');
     element.children[1].textContent = 1;
+    addEffectCart();
     //Eliminar producto del array
     removeFromCart(name);
   } else {
     cartProduct.quantity--;
+    cartAddProductsText();
   }
 
-  console.log(cartContainer);
+  //console.log(cartContainer);
 
   cartTextButton(element, cartProduct.quantity);
 };
 
 const showEventsButton = (effectVisual, name, price) => {
   const buttonHidden = effectVisual; //hidden button
-  buttonHidden.classList.add("cart-container-hidden");
+  buttonHidden.classList.add('cart-container-hidden');
 
   const buttonShow = effectVisual.nextElementSibling; //show button
-  buttonShow.classList.remove("hide");
+  buttonShow.classList.remove('hide');
 
   const imgBorder = effectVisual.previousElementSibling; //dessert selector border
-  imgBorder.classList.add("border-img-select");
+  imgBorder.classList.add('border-img-select');
 
   addToCart(name, price);
 };
 
-const containerObjetcs = (event) => {
+const containerObjetcs = event => {
   const type = event.target.dataset.type;
   //console.log(type);
   if (!type) return;
@@ -99,18 +119,18 @@ const containerObjetcs = (event) => {
   const nameDessert = event.target.dataset.name;
   const priceDessert = event.target.dataset.price;
 
-  if (type === "add") {
+  if (type === 'add') {
     showEventsButton(event.target, nameDessert, priceDessert);
     //console.log(event.target.dataset);
   }
 
-  if (type === "increase") {
+  if (type === 'increase') {
     const nameDessert = event.target.parentElement.dataset.name;
     const priceDessert = event.target.parentElement.dataset.price;
 
     increaseQuantity(event.target.parentElement, nameDessert, priceDessert);
     cartAddProductsText(event.target.parentElement.nameDessert);
-  } else if (type === "decrease") {
+  } else if (type === 'decrease') {
     const nameDessert = event.target.parentElement.dataset.name;
     const priceDessert = event.target.parentElement.dataset.price;
 
@@ -118,8 +138,8 @@ const containerObjetcs = (event) => {
   }
 };
 
-optionsFilterElement.addEventListener("click", filterSelection);
-containerObjetcsElement.addEventListener("click", containerObjetcs);
+optionsFilterElement.addEventListener('click', filterSelection);
+containerObjetcsElement.addEventListener('click', containerObjetcs);
 
 //<div class="cart-full cart-full-hide">
 //<h2 class="cart-title">Your Cart (0)</h2>
