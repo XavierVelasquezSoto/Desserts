d83365f2692a4f07ce956ade415e11c97940147a
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-28 01:32:16 +0100

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
-const optionsFilterElement = document.getElementById('options-filter');
-const containerObjetcsElement = document.getElementById('container-objetcs');
-const cartProductsElement = document.getElementById('cart-products');
-const cartImgElement = document.getElementById('cart-img');
-const cartOrderTotalPriceElement = document.getElementById('cart-order-total-price');
-const totalOrderContainerElement = document.getElementById('total-order-container');
-const accumulateProductsCartElement = document.getElementById('accumulate-products-cart');
+const optionsFilterElement = document.getElementById("options-filter");
+const containerObjetcsElement = document.getElementById("container-objetcs");
+const cartProductsElement = document.getElementById("cart-products");
+const cartImgElement = document.getElementById("cart-img");
+const cartOrderTotalPriceElement = document.getElementById(
+  "cart-order-total-price"
+);
+const totalOrderContainerElement = document.getElementById(
+  "total-order-container"
+);
+const accumulateProductsCartElement = document.getElementById(
+  "accumulate-products-cart"
+);
 /* alternar comentario de linea = añadir tecla ctrl + }  si no lo tiene*/
 
 let cartContainer = [];
 
-const filterSelection = event => {
+const filterSelection = (event) => {
   event.target.dataset.filters;
   //console.log(event.target.dataset.filters);
   if (!event.target.dataset.filters) {
@@ -17,16 +23,72 @@ const filterSelection = event => {
   }
 
   for (const filters of optionsFilterElement.children) {
-    filters.classList.remove('filter-desserts-active');
+    filters.classList.remove("filter-desserts-active");
   }
-  event.target.classList.add('filter-desserts-active');
+  event.target.classList.add("filter-desserts-active");
 };
 
-//<div class="cart-full cart-full-hide">
-//<h2 class="cart-title">Your Cart (0)</h2>
-//<span class="cart-description-dessert">Classic Tiramisu</span>
-//<div class="cart-total">
-//<span class="cart-add">1x</span>
+const addProducts = () => {
+  const fragment = document.createDocumentFragment();
+  const productsEach = cartContainer.forEach((product) => {
+    //
+    const newCartFull = document.createElement("div");
+    newCartFull.classList.add("cart-full");
+
+    const newCartDescriptionDessert = document.createElement("span");
+    newCartDescriptionDessert.classList.add("cart-description-dessert");
+    newCartDescriptionDessert.textContent = `${product.name}`;
+
+    const newCartTotal = document.createElement("div");
+    newCartTotal.classList.add("cart-total");
+    newCartTotal.textContent = ``;
+
+    const newCartAdd = document.createElement("span");
+    newCartAdd.classList.add("cart-add");
+    newCartAdd.textContent = `${product.quantity}`;
+
+    const newCartPrice = document.createElement("span");
+    newCartPrice.classList.add("cart-price");
+    newCartPrice.textContent = `@ $${product.price}`;
+
+    const newcartAccumulatedPrice = document.createElement("span");
+    newcartAccumulatedPrice.classList.add("cart-accumulated-price");
+    newcartAccumulatedPrice.textContent = `$${product.price * product.price}`;
+
+    const newCartRemoveDessert = document.createElement("img");
+    newCartRemoveDessert.classList.add("cart-remove-dessert");
+    newCartRemoveDessert.src = `./assets/img/icon-remove-item.svg`;
+
+    // const newCartNeutralGreen = document.createElement("div");
+    // newCartNeutralGreen.classList.add("cart-neutral-green");
+
+    // const newCartNeutralGreenImg = document.createElement("img");
+    // newCartNeutralGreenImg.src = `./assets/img/icon-carbon-neutral.svg`;
+
+    // const newCarbonNeutral = document.createElement("span");
+    // newCarbonNeutral.classList.add("carbon-neutral");
+
+    // const newCartBuyOrder = document.createElement("button");
+    // newCartBuyOrder.classList.add("cart-buy-order");
+
+    newCartFull.append(newCartDescriptionDessert, newCartTotal); // span name deesert product y div container product
+    newCartTotal.append(
+      newCartAdd,
+      newCartPrice,
+      newcartAccumulatedPrice,
+      newCartRemoveDessert
+    );
+
+    fragment.append(newCartFull, newCartDescriptionDessert, newCartTotal);
+  });
+  accumulateProductsCartElement.append(fragment);
+};
+
+//<div class="cart-full cart-full-hide">---
+//<h2 class="cart-title">Your Cart (0)</h2>---
+//<span class="cart-description-dessert">Classic Tiramisu</span>--
+//<div class="cart-total">--
+//<span class="cart-add">1x</span> --
 //<span class="cart-price">@ $5.50</span>
 //<span class="cart-accumulated-price">$5.50</span>
 //<img
@@ -48,26 +110,18 @@ const filterSelection = event => {
 //<button class="cart-buy-order">Confirm Order</button>
 //</div>;
 
-const addProducts = () => {
-  //const fragment = document.createDocumentFragment()
-  const newPrintProducts = cartContainer.forEach(printProducts => {
-    newPrintProducts = document.createElement('span');
-    newPrintProducts;
-    document.body.append(accumulateProductsCartElement);
-  });
-};
-
 const removeEffectCartAdd = () => {
-  cartImgElement.classList.add('hide');
+  cartImgElement.classList.add("hide");
 };
 
 const valorProductEffect = () => {
   if (cartContainer.length === 0) {
-    cartImgElement.classList.remove('hide');
-    totalOrderContainerElement.classList.add('hide');
+    cartImgElement.classList.remove("hide");
+    totalOrderContainerElement.classList.add("hide");
   } else {
-    cartImgElement.classList.add('hide');
-    totalOrderContainerElement.classList.remove('hide');
+    cartImgElement.classList.add("hide");
+    totalOrderContainerElement.classList.remove("hide");
+    addProducts();
   }
 };
 
@@ -100,8 +154,8 @@ const addToCart = (name, price) => {
   valorProductEffect();
 };
 
-const removeFromCart = name => {
-  cartContainer = cartContainer.filter(productCart => {
+const removeFromCart = (name) => {
+  cartContainer = cartContainer.filter((productCart) => {
     return productCart.name !== name;
   });
   cartAddProductsText();
@@ -110,7 +164,7 @@ const removeFromCart = name => {
 };
 
 const increaseQuantity = (element, name) => {
-  const cartProduct = cartContainer.find(product => {
+  const cartProduct = cartContainer.find((product) => {
     return product.name === name;
   });
   cartProduct.quantity++;
@@ -122,14 +176,16 @@ const increaseQuantity = (element, name) => {
 
 const decreaseQuantity = (element, name) => {
   //console.log(element, name);
-  const cartProduct = cartContainer.find(product => {
+  const cartProduct = cartContainer.find((product) => {
     return product.name === name;
   });
 
   if (cartProduct.quantity === 1) {
-    element.previousElementSibling.classList.remove('cart-container-hidden');
-    element.previousElementSibling.previousElementSibling.classList.remove('border-img-select');
-    element.classList.add('hide');
+    element.previousElementSibling.classList.remove("cart-container-hidden");
+    element.previousElementSibling.previousElementSibling.classList.remove(
+      "border-img-select"
+    );
+    element.classList.add("hide");
     element.children[1].textContent = 1;
     //Eliminar producto del array
     removeFromCart(name);
@@ -146,18 +202,18 @@ const decreaseQuantity = (element, name) => {
 
 const showEventsButton = (effectVisual, name, price) => {
   const buttonHidden = effectVisual; //hidden button
-  buttonHidden.classList.add('cart-container-hidden');
+  buttonHidden.classList.add("cart-container-hidden");
 
   const buttonShow = effectVisual.nextElementSibling; //show button
-  buttonShow.classList.remove('hide');
+  buttonShow.classList.remove("hide");
 
   const imgBorder = effectVisual.previousElementSibling; //dessert selector border
-  imgBorder.classList.add('border-img-select');
+  imgBorder.classList.add("border-img-select");
 
   addToCart(name, price);
 };
 
-const containerObjetcs = event => {
+const containerObjetcs = (event) => {
   const type = event.target.dataset.type;
   //console.log(type);
   if (!type) return;
@@ -165,18 +221,18 @@ const containerObjetcs = event => {
   const nameDessert = event.target.dataset.name;
   const priceDessert = event.target.dataset.price;
 
-  if (type === 'add') {
+  if (type === "add") {
     showEventsButton(event.target, nameDessert, priceDessert);
     //console.log(event.target.dataset);
   }
 
-  if (type === 'increase') {
+  if (type === "increase") {
     const nameDessert = event.target.parentElement.dataset.name;
     const priceDessert = event.target.parentElement.dataset.price;
 
     increaseQuantity(event.target.parentElement, nameDessert, priceDessert);
     cartAddProductsText(event.target.parentElement.nameDessert);
-  } else if (type === 'decrease') {
+  } else if (type === "decrease") {
     const nameDessert = event.target.parentElement.dataset.name;
     const priceDessert = event.target.parentElement.dataset.price;
 
@@ -184,5 +240,5 @@ const containerObjetcs = event => {
   }
 };
 
-optionsFilterElement.addEventListener('click', filterSelection);
-containerObjetcsElement.addEventListener('click', containerObjetcs);
+optionsFilterElement.addEventListener("click", filterSelection);
+containerObjetcsElement.addEventListener("click", containerObjetcs);
